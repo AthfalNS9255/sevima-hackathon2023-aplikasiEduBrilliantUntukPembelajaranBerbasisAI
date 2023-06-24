781ddafc9414d1bcca486441c5f05a6c7a702f23
Athfal <Athfal@DESKTOP-Q7NDM0F>
2023-06-25 05:50:58 +0700

    Commit

---
+++ UserControlsGuru/UCDashboardGuru.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace EduBrilliant.UserControls
+{
+	partial class UCDashboardGuru
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.guna2ShadowPanel2 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+			this.lblTransaksi = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+			this.lblKelas = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.guna2ShadowPanel2.SuspendLayout();
+			this.guna2ShadowPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.Controls.Add(this.guna2ShadowPanel2, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.guna2ShadowPanel1, 0, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(669, 112);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// guna2ShadowPanel2
+			// 
+			this.guna2ShadowPanel2.BackColor = System.Drawing.Color.Transparent;
+			this.guna2ShadowPanel2.Controls.Add(this.lblTransaksi);
+			this.guna2ShadowPanel2.Controls.Add(this.label3);
+			this.guna2ShadowPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.guna2ShadowPanel2.FillColor = System.Drawing.Color.MediumTurquoise;
+			this.guna2ShadowPanel2.Location = new System.Drawing.Point(339, 5);
+			this.guna2ShadowPanel2.Margin = new System.Windows.Forms.Padding(5);
+			this.guna2ShadowPanel2.Name = "guna2ShadowPanel2";
+			this.guna2ShadowPanel2.Radius = 3;
+			this.guna2ShadowPanel2.ShadowColor = System.Drawing.Color.Black;
+			this.guna2ShadowPanel2.Size = new System.Drawing.Size(325, 102);
+			this.guna2ShadowPanel2.TabIndex = 1;
+			// 
+			// lblTransaksi
+			// 
+			this.lblTransaksi.AutoSize = true;
+			this.lblTransaksi.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTransaksi.Location = new System.Drawing.Point(10, 51);
+			this.lblTransaksi.Name = "lblTransaksi";
+			this.lblTransaksi.Size = new System.Drawing.Size(26, 31);
+			this.lblTransaksi.TabIndex = 1;
+			this.lblTransaksi.Text = "1";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(10, 10);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(106, 31);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Transaksi";
+			// 
+			// guna2ShadowPanel1
+			// 
+			this.guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent;
+			this.guna2ShadowPanel1.Controls.Add(this.lblKelas);
+			this.guna2ShadowPanel1.Controls.Add(this.label1);
+			this.guna2ShadowPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.guna2ShadowPanel1.FillColor = System.Drawing.Color.DarkKhaki;
+			this.guna2ShadowPanel1.Location = new System.Drawing.Point(5, 5);
+			this.guna2ShadowPanel1.Margin = new System.Windows.Forms.Padding(5);
+			this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
+			this.guna2ShadowPanel1.Radius = 3;
+			this.guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Black;
+			this.guna2ShadowPanel1.Size = new System.Drawing.Size(324, 102);
+			this.guna2ShadowPanel1.TabIndex = 0;
+			// 
+			// lblKelas
+			// 
+			this.lblKelas.AutoSize = true;
+			this.lblKelas.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblKelas.Location = new System.Drawing.Point(10, 51);
+			this.lblKelas.Name = "lblKelas";
+			this.lblKelas.Size = new System.Drawing.Size(26, 31);
+			this.lblKelas.TabIndex = 1;
+			this.lblKelas.Text = "1";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(10, 10);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(67, 31);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Kelas";
+			// 
+			// UCDashboardGuru
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "UCDashboardGuru";
+			this.Size = new System.Drawing.Size(675, 579);
+			this.Load += new System.EventHandler(this.UCDashboardGuru_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.guna2ShadowPanel2.ResumeLayout(false);
+			this.guna2ShadowPanel2.PerformLayout();
+			this.guna2ShadowPanel1.ResumeLayout(false);
+			this.guna2ShadowPanel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+		private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel2;
+		private System.Windows.Forms.Label lblTransaksi;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label lblKelas;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ UserControlsGuru/UCDashboardGuru.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EduBrilliant.UserControls
+{
+	public partial class UCDashboardGuru : UserControl
+	{
+		EduBrilliantEntities db = new EduBrilliantEntities();
+		public UCDashboardGuru()
+		{
+			InitializeComponent();
+		}
+
+		private void UCDashboardGuru_Load(object sender, EventArgs e)
+		{
+			int Step = 0;
+			try
+			{
+				int KelasCount = db.Transaksis.Where(x => x.UserID == Session.UserID).GroupBy(x => x.KelasID).Count();
+				lblKelas.Text = KelasCount.ToString();
+
+				int TransaksiCount = db.Transaksis.Where(x => x.UserID == Session.UserID).Count();
+				lblTransaksi.Text = TransaksiCount.ToString();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Error Load Step {Step} = {ex.Message}");
+			}
+		}
+
+		//private void guna2ShadowPanel1_Click(object sender, EventArgs e)
+		//{
+		//	FrmMainAdmin frm = new FrmMainAdmin();
+		//	UCGuru uc = new UCGuru();
+		//	frm.Navigation(uc, frm.btnGuru);
+		//}
+
+		//private void guna2ShadowPanel2_Click(object sender, EventArgs e)
+		//{
+		//	FrmMainAdmin frm = new FrmMainAdmin();
+		//	UCSiswa uc = new UCSiswa();
+		//	frm.Navigation(uc, frm.btnSiswa);
+		//}
+
+		//private void guna2ShadowPanel3_Click(object sender, EventArgs e)
+		//{
+		//	FrmMainAdmin frm = new FrmMainAdmin();
+		//	UCKategori uc = new UCKategori();
+		//	frm.Navigation(uc, frm.btnKategori);
+		//}
+	}
+}
